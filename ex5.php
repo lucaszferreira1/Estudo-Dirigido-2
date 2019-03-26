9b67f562cbfa1272c184b8bbd4c1cf8e3e1f4a1e
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-26 10:42:50 -0300

    Início e Fim

---
+++ ex5.php
@@ -0,0 +1,19 @@
+<?php
+
+print "De sua primeira nota:";
+$a=(float)fgets(STDIN);
+
+print "De sua segunda nota:";
+$b=(float)fgets(STDIN);
+
+$media=($a+$b)/2;
+
+if ($media>=7 and $media<10){
+    print "Aprovado\n";
+}
+if ($media<7){
+    print "Reprovado\n";
+}
+if ($media==10){
+    print "Aprovado com Distinção\n";
+}
