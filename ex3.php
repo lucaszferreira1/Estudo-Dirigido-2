49fbca40120ce9447f2aa5ef7f8b01d71ad65ae5
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-26 10:50:09 -0300

    Mais ou Menos

--- ex3.php
+++ ex3.php
@@ -1,1 +1,14 @@
 <?php
+
+print "Digite um letra: M ou F:";
+$sexo=(string)fgets(STDIN);
+
+if ($sexo=="m"){
+    print "Masculino";
+}
+elseif($sexo=="f"){
+    print"Feminino";
+}
+elseif($sexo<>"m" and $sexo<>"f"){
+    print "Sexo Invalido";
+}
