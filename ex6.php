d39be19b6457a25df75941aa2e461c7b301d4f4f
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-26 11:01:12 -0300

    Início e Fim

---
+++ ex6.php
@@ -0,0 +1,20 @@
+<?php
+
+print "Digite um número:";
+$nu1=(float)fgets(STDIN);
+
+print "Digite um número:";
+$nu2=(float)fgets(STDIN);
+
+print "Digite um número:";
+$nu3=(float)fgets(STDIN);
+
+if ($nu1>$nu2 and $nu1>$nu3){
+    print "O primeiro número é o maior\n";
+}
+if ($nu2>$nu1 and $nu2>$nu3){
+    print "O segundo é o maior\n";
+}
+if ($nu3>$nu1 and $nu3>$nu2){
+    print "O terceiro é o maior\n";
+}
