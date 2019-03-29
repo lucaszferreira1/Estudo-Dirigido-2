8fcddca136c4f7c3869dd3d27bef1761b8121153
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-29 16:06:30 -0300

    Início

---
+++ ex10.php
@@ -0,0 +1,15 @@
+<?php
+
+print "Qual o valor de A:";
+$a=(float)fgets(STDIN);
+
+if ($a=0){
+    exit (0);
+}
+
+print "Qual o valor de B:";
+$b=(float)fgets(STDIN);
+
+print "Qual o valor de C:";
+$c=(float)fgets(STDIN);
+
