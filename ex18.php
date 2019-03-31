67ae012094c16f622b662129f23f3067ac430e4c
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-31 19:20:24 -0300

    Início

---
+++ ex18.php
@@ -0,0 +1,51 @@
+</php 
+
+print "Digite um número:";
+$num1=(int)fgets(STDIN);
+
+if ($num1)
+
+print "Digite um número:";
+$num2=(int)fgets(STDIN);
+
+
+
+print "Digite um número:";
+$num3=(int)fgets(STDIN);
+
+
+
+print "Digite um número:";
+$num4=(int)fgets(STDIN);
+
+
+
+print "Digite um número:";
+$num5=(int)fgets(STDIN);
+
+
+
+print "Digite um número:";
+$num6=(int)fgets(STDIN);
+
+
+
+print "Digite um número:";
+$num7=(int)fgets(STDIN);
+
+
+
+print "Digite um número:";
+$num8=(int)fgets(STDIN);
+
+
+
+print "Digite um número:";
+$num9=(int)fgets(STDIN);
+
+
+
+print "Digite um número:";
+$num10=(int)fgets(STDIN);
+
+
