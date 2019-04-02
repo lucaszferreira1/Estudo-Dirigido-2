e2fa4f9534b10047e5acc550278a92cc4fff9629
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-04-02 09:50:53 -0300

    Resultado Final

--- ex10.php
+++ ex10.php
@@ -1,31 +1,27 @@
 <?php
-
 print "Qual o valor de A:";
 $a=(float)fgets(STDIN);
-
-if ($a=0){
-    exit (0);
+if ($a==0){
+    exit;
 }
-
 print "Qual o valor de B:";
 $b=(float)fgets(STDIN);
-
 print "Qual o valor de C:";
 $c=(float)fgets(STDIN);
-
-$delta=(pow(b,2)-4*($a*$c));
+$delta=(pow($b,2)-(4*$a*$c));
     if ($delta>0){
         print "Há duas raízes reais\n";
     }
     elseif ($delta<0){
         print "Não há raízes reais\n";
-        exit(0);
+        exit;
     }
-    elseif ($delta=0){
+    elseif ($delta==0){
         print "Só há uma raíz real\n";
-    }
-    $x1=(-($b)+$delta)/(2*$a);
-    $x2=(-($b)-$delta)/(2*$a);
 
+    }
+    $x1=((-($b)+sqrt($delta))/(2*$a));
+    $x2=((-($b)-sqrt($delta))/(2*$a));
 print "X1 = $x1\n";
 print "X2 = $x2\n";
+
