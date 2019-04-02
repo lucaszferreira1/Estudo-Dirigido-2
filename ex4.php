b5d8e4a22fb7d66a34ac161a4154af4cef710466
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-04-02 10:25:17 -0300

    Resultado Final

--- ex4.php
+++ ex4.php
@@ -1,1 +1,11 @@
 <?php
+
+print "Digite um letra:";
+$letra=(string)fgetc(STDIN);
+
+if ($letra=="a" or $letra=="e" or $letra=="i" or $letra=="o" or $letra=="u"){
+    print "Vogal\n";
+}
+else{
+    print"Consoante\n";
+}
