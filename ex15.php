40c4073c89b7f0b468b9b7e32b5405bf95cc17e1
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-04-02 09:58:54 -0300

    Resultado Final

--- ex15.php
+++ ex15.php
@@ -1,4 +1,4 @@
-</php
+<?php
 
 print "Digite o primeiro número:";
 $num1=(float)fgets(STDIN);
@@ -16,21 +16,21 @@ print "Digite o quinto número:";
 $num5=(float)fgets(STDIN);
 
 if ($num1>$num2 and $num1>$num3 and $num1>$num4 and $num1>$num5){
-print "O primeiro número é o maior";
+print "O Primeiro número é o maior";
 }
 
 if ($num2>$num1 and $num2>$num3 and $num2>$num4 and $num2>$num5){
-print "O primeiro segundo é o maior";
+print "O Segundo segundo é o maior";
 }
 
 if ($num3>$num1 and $num3>$num2 and $num3>$num4 and $num3>$num5){
-print "O primeiro terceiro é o maior";
+print "O Terceiro terceiro é o maior";
 }
 
 if ($num4>$num1 and $num4>$num2 and $num4>$num3 and $num4>$num5){
-print "O primeiro número é o maior";
+print "O Quarto número é o maior";
 }
 
 if ($num5>$num2 and $num5>$num3 and $num5>$num4 and $num5>$num1){
-print "O primeiro número é o maior";
+print "O Quinto número é o maior";
 }
