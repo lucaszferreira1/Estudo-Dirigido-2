1f77e2393e16b034c68058c47fab5d348501961b
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-04-02 10:20:37 -0300

    Resultado Final

--- ex18.php
+++ ex18.php
@@ -1,51 +1,22 @@
-</php 
+<?php 
 
-print "Digite um número:";
-$num1=(int)fgets(STDIN);
-
-if ($num1)
-
-print "Digite um número:";
-$num2=(int)fgets(STDIN);
+$vezes=0;
+$numimpares=0;
+$numpares=0;
 
 
+while ($vezes<>10){
+    print "Digite um número:";
+    $num=(int)fgets(STDIN);
+    $vezes=$vezes+1;
+    if ($num%2==0){
+        $numpares=$numpares+1;
+    }
+    else {
+        $numimpares=$numimpares+1;
+    }
+}
 
-print "Digite um número:";
-$num3=(int)fgets(STDIN);
-
-
-
-print "Digite um número:";
-$num4=(int)fgets(STDIN);
-
-
-
-print "Digite um número:";
-$num5=(int)fgets(STDIN);
-
-
-
-print "Digite um número:";
-$num6=(int)fgets(STDIN);
-
-
-
-print "Digite um número:";
-$num7=(int)fgets(STDIN);
-
-
-
-print "Digite um número:";
-$num8=(int)fgets(STDIN);
-
-
-
-print "Digite um número:";
-$num9=(int)fgets(STDIN);
-
-
-
-print "Digite um número:";
-$num10=(int)fgets(STDIN);
-
+print "Há $numimpares número(s) ímpare(s)\n";
+print "Há $numpares número(s) pare(s)\n";
 
