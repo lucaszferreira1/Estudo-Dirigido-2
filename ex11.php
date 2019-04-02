5606064d97843f95399be345e9a277b55aea0170
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-04-02 10:38:56 -0300

    Resultado Final

--- ex11.php
+++ ex11.php
@@ -1,5 +1,33 @@
-</php 
+<?php
 
-print "Digite um número inteiro menor que 1000";
-$num=(int)fgets(STDIN);
+$centena=0;
+$dezena=0;
+$unidade=0;
 
+print "Digite um número entre 0-999:";
+$zum= (int)fgets(STDIN);
+$num= $zum;
+
+    $centena=(int)($zum/100)%100;
+    $dezena= (int)($zum/10)%10;
+    $unidade=(int)($zum%100)%10;
+
+    if ($centena>1 or $centena==0){
+        print "$centena Centenas ";
+    }
+    elseif ($centena==1){
+        print "$centena Centena ";
+    }
+
+    if ($dezena>1 or $dezena==0){
+        print "$dezena Dezenas ";
+    }
+    elseif ($dezena==1){
+        print "$dezena Dezena ";
+    }
+    if ($unidade>1 or $unidade==0){
+        print "e $unidade Unidades \n";
+    }
+    elseif ($unidade==1){
+        print "e $unidade Unidade \n";
+    }
